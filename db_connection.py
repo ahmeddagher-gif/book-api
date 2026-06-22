8922b71bdf68014549c46a58694804da738aff9e
ahmed dagher <ahmedlena2009@gmail.com>
2026-06-22 19:15:58 +0300

    the secrets actions error

--- db_connection.py
+++ db_connection.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL=os.getenv('DATABASE_URL')
+DATABASE_URL=os.getenv('DATABASE_URL')# i hate you 
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
